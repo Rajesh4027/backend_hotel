2ca4b7f3b9cd88170aab49aad4be7f6bd24bc17e
Rajesh4027 <rajeshdevaraj44@gmail.com>
2025-11-25 14:14:45 +0530

    first commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: ['https://maduragrandeur.netlify.app', 'http://localhost:5500', 'http://127.0.0.1:5500'],
+    origin: ['https://maduragrandeur1.netlify.app', 'http://localhost:5500', 'http://127.0.0.1:5500'],
     credentials: true
 }));
 
